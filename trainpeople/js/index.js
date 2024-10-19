4a3c538711021615705d5fa38101a4abdaed2b98
Крутой <135390587+kruroi@users.noreply.github.com>
2024-10-19 10:55:47 +1000

    Add files via upload

---
+++ trainpeople/js/index.js
@@ -0,0 +1,34 @@
+
+
+document.getElementById("button-close").addEventListener("click", () => {
+    document.getElementById("div-rod").style.display = "none" 
+    document.body.style.position = "static"
+})
+
+document.getElementById("close-button").addEventListener("click", () => {
+    document.getElementById("poisk").style.display = "none" 
+    document.body.style.position = "static"
+})
+
+document.getElementById("button-poisk").addEventListener("click", () => {
+    var a = parseInt(document.getElementById('poisk-value').value);
+    document.getElementById("poisk").style.display = "none" 
+    document.getElementById("div-info").style.display = "block"
+    document.getElementById("u-num").innerHTML = a
+})
+
+document.getElementById("CloseDivInfo").addEventListener("click", () => {
+    document.getElementById("div-info").style.display = "none" 
+    document.body.style.position = "static"
+})
+
+function reg(){
+    document.getElementById("div-rod").style.display = "block"
+    document.body.style.position = "fixed"
+}
+
+
+function poisk(){
+    document.getElementById("poisk").style.display = "block"
+    document.body.style.position = "fixed"
+}
